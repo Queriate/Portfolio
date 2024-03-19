fe6369c00497b3b2c0a3cb875d70e05e4551c30d
Queriate Lecter <queriatejobs@gmail.com>
2024-03-19 11:23:16 -0300

    novo codigo para apenas um botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,12 @@
-function toggleLanguage(language) {
+function toggleLanguage() {
   var elementsEn = document.querySelectorAll('.lang-en');
   var elementsPtBr = document.querySelectorAll('.lang-pt-br');
-  
-  if (language === 'en') {
-      elementsEn.forEach(function(element) {
-          element.style.display = 'block';
-      });
-      elementsPtBr.forEach(function(element) {
-          element.style.display = 'none';
-      });
-  } else if (language === 'pt-br') {
-      elementsEn.forEach(function(element) {
-          element.style.display = 'none';
-      });
-      elementsPtBr.forEach(function(element) {
-          element.style.display = 'block';
-      });
-  }
+
+  elementsEn.forEach(function(element) {
+      element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+  });
+
+  elementsPtBr.forEach(function(element) {
+      element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+  });
 }
